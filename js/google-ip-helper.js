cb78f3c281603d75c66db3b44c514e20aa9c76d8
Linpeng <magixsouce.cn@gmail.com>
2014-06-18 10:59:23 +0800

    Signed-off-by: Linpeng <magixsouce.cn@gmail.com>

---
+++ js/google-ip-helper.js
@@ -0,0 +1,23 @@
+function ping(jq){
+  var img = new Image();
+  var ip = jq.text(); 
+  img.src = "http://"+ip+"/textinputassistant/tia.png"; 
+  
+  img.onload = function(){  
+    jq.addClass('green');
+  }
+}
+
+$(function(){
+    $('#ping_btn').click(function(){
+        $('#ping_btn').attr('value','Processing');
+        $('td > a').each(function(i){
+            var $this = $(this);
+            var text = $this.text();
+            var array = text.split("."); 
+            if(array && array.length==4){
+                ping($this);
+            }
+        });
+    });
+});
